b4b9339f8cfdb78a236572e9cee5da97da828a6f
app-kaisha <app.kaihatsusha@gmail.com>
2026-06-01 19:37:32 +0100

    Initial Commit

---
+++ DisneyCharacters/ContentView.swift
@@ -0,0 +1,25 @@
+//
+//  ContentView.swift
+//  DisneyCharacters
+//
+//  Created by app-kaihatsusha on 01/06/2026.
+//  Copyright © 2026 app-kaihatsusha. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ DisneyCharacters/DisneyCharactersApp.swift
@@ -0,0 +1,18 @@
+//
+//  DisneyCharactersApp.swift
+//  DisneyCharacters
+//
+//  Created by app-kaihatsusha on 01/06/2026.
+//  Copyright © 2026 app-kaihatsusha. All rights reserved.
+//
+
+import SwiftUI
+
+@main
+struct DisneyCharactersApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
